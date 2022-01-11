caf40b82a58f5eab08d0f336be4257d50a8ea573
Kohei Otsuka <kochizufan@gmail.com>
2022-01-11 11:30:22 +0900

    PWA設定

---
+++ sw.js
@@ -0,0 +1,46 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js');
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+workbox.navigationPreload.enable();
+
+// ------------------  runtime caching starts ---------------------
+
+// splash icon images
+workbox.routing.registerRoute(
+  new RegExp('/(?:splashscreens|icons)/.*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'icons',
+    maxEntries: 50,
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30, // 1month
+        purgeOnQuotaError: true
+      }),
+    ],
+  })
+);
+
+// manifest
+workbox.routing.registerRoute(
+  new RegExp('manifest.json'),
+  new workbox.strategies.StaleWhileRevalidate()
+);
+
+// ------------------  precaching the assets ---------------------
+workbox.precaching.precacheAndRoute([
+  // html
+  'index.html',
+  // js
+  'https://unpkg.com/harumi@0.0.2/dist/harumi.min.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+  // css
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+  // font
+  './fonts/genkai-sub.woff',
+  './fonts/genkai-sub.woff2',
+  // other
+  'manifest.json'
+]);
