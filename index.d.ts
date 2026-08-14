d7601eb675165e043fece93049f703f8667bae5a
kochizufan <kochizufan@users.noreply.github.com>
2026-08-14 21:56:18 +0000

    deploy: bc3829a16a548be33235495a85047489c5231052

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,3 @@
 import { SearchOptions, SearchResult } from './types';
-
 export declare function ambiguousSearch(text: string, options?: SearchOptions): SearchResult[];
 export type { SearchOptions, SearchResult };
