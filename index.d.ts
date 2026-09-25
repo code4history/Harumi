552e280a9a5ef9759f0ae2fa2c6b63d6ca25e3a6
kochizufan <kochizufan@users.noreply.github.com>
2026-09-25 23:07:47 +0000

    deploy: 3ee89020c44fd0ee74597f2613c96126a9c839a0

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,3 @@
 import { SearchOptions, SearchResult } from './types';
 export declare function ambiguousSearch(text: string, options?: SearchOptions): SearchResult[];
-export declare function nengoNames(text: string, options?: SearchOptions): string[];
 export type { SearchOptions, SearchResult };
